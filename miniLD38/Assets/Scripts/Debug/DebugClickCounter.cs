95d83f075e9ba0243911fab3aa5eb1d3b6136492
Bohdan Taraba <tbohdan@seznam.cz>
2017-02-19 12:41:37 +0100

    created first button counter

---
+++ miniLD38/Assets/Scripts/Debug/DebugClickCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugClickCounter : MonoBehaviour {
+
+    Text clickCount;
+    ClickController click;
+
+    void Start () {
+        click = GameObject.FindGameObjectWithTag("GameController").GetComponent<ClickController>();
+        clickCount = GetComponent<Text>();
+	}
+	
+	void Update () {
+        clickCount.text = ClickController.clickCount.ToString();//click.clickCount.ToString();
+	}
+}
---
+++ miniLD38/Assets/Scripts/GameManager/ClickController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickController : MonoBehaviour {
+
+    public static int clickCount = 0;
+
+    void Awake()
+    {
+        clickCount = 0;
+    }
+
+	void Start () {
+        print(clickCount);
+	}
+
+	void Update () {
+	}
+
+    public void ButtonClick()
+    {
+        clickCount++;
+    }
+}
