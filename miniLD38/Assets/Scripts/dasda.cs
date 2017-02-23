aaba0eaf5dc78bbe6ae57bc2df20b5bb09098bd4
Jakub Gereg <j.gereg@gmail.com>
2017-02-23 20:41:25 +1300

    test script added

---
+++ miniLD38/Assets/Scripts/dasda.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class dasda : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        MoveBelt(col);
+    }
+
+    void MoveBelt(Collision2D col)
+    {
+        var gm = col.gameObject;
+        var rigid = gm.GetComponent<Rigidbody2D>();
+        if (gm.tag == "part")
+        {
+            rigid.velocity = Vector2.right * 5;
+        }
+    }
+    void OnCollisionStay2D(Collision2D col)
+    {
+        MoveBelt(col);
+
+
+    }
+}
