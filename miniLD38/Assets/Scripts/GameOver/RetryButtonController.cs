d94b571ac04c9c2db1d9aac628a30fe9a3709835
Bohdan Taraba <tbohdan@seznam.cz>
2017-02-28 12:01:53 +0100

    variou changes in scenes

---
+++ miniLD38/Assets/Scripts/GameOver/RetryButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButtonController : MonoBehaviour {
+
+    public string gameSceneName;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playAgain()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
